d3c45122e8686708082aabf7018b76c69c40058a
jim <chemuku.jimmy@gmail.com>
2026-02-21 13:52:12 +0300

    feat(page): Home
    Added a component that hosts all the app's screens

---
+++ src/screens/home.tsx
@@ -0,0 +1,14 @@
+import { View } from 'react-native';
+import { JSX } from 'react';
+
+/**
+ * Home page
+ * @returns {JSX.Element} A reusable component that renders the home page
+ */
+const Home = (): JSX.Element => {
+  return <View style={{flex: 1}}>
+  </View>;
+};
+
+
+export default Home;
