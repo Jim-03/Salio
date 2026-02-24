e3dc8368dfe0a7ea0947832e04f1fdab061f05c3
jim <chemuku.jimmy@gmail.com>
2026-02-24 16:26:57 +0300

    feat: Enhance Main component with SMS import and Home screen rendering

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,8 +1,10 @@
-import { Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import NavBar from '../components/navBar';
-import useAppStyles from '../utils/styles';
-import { useState } from 'react';
+import { AppState, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import NavBar from "../components/navBar";
+import useAppStyles from "../utils/styles";
+import { useEffect, useState } from "react";
+import Home from "../screens/home";
+import { useSms } from "../services/messages";
 
 /**
  * Main page
@@ -10,16 +12,27 @@ import { useState } from 'react';
  */
 const Main = () => {
   const appStyles = useAppStyles();
-  const [activeButton, setActiveButton] = useState('Home');
+  const [activeButton, setActiveButton] = useState("Home");
+  const { importSms } = useSms();
 
-  return <View style={{flex: 1, backgroundColor: 'seagreen'}}>
-    <SafeAreaView>
-      <View style={[{alignItems: 'center'}, appStyles.screenBackground]}>
-        <Text>Hello</Text>
-        <NavBar activeButton={activeButton} setActiveButton={setActiveButton}/>
-      </View>
-    </SafeAreaView>
-  </View>;
+  useEffect(() => {
+    importSms();
+  }, []);
+  return (
+    <View style={{ flex: 1, backgroundColor: "seagreen" }}>
+      <SafeAreaView>
+        <View style={[{ alignItems: "center" }, appStyles.screenBackground]}>
+          <View style={{ flex: 1, width: "95%", margin: 5 }}>
+            {activeButton === "Home" && <Home />}
+          </View>
+          <NavBar
+            activeButton={activeButton}
+            setActiveButton={setActiveButton}
+          />
+        </View>
+      </SafeAreaView>
+    </View>
+  );
 };
 
 export default Main;
