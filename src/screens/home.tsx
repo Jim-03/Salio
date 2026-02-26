20f62b5c860d94a1e5bf82f7842a33b2d36219d2
Jim-03 <chemuku.jimmy@gmail.com>
2026-02-26 10:00:37 +0300

    feat: Add ExpenseSummary component to Home screen

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,16 +1,17 @@
 import { View } from "react-native";
 import { JSX } from "react";
 import HomeBanner from "../components/homeBanner";
+import ExpenseSummary from "../components/expenseSummary";
 
 /**
  * Home page
  * @returns {JSX.Element} A reusable component that renders the home page
  */
 const Home = (): JSX.Element => {
-
   return (
     <View style={{ flex: 1 }}>
       <HomeBanner />
+      <ExpenseSummary />
     </View>
   );
 };
