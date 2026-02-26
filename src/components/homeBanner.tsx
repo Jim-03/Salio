662464a0917d6d6d5cbe9f0eb4b9e81955363174
jim <chemuku.jimmy@gmail.com>
2026-02-26 18:42:47 +0300

    feat: Update HomeBanner component to use ActivityIndicator and improve locale formatting

--- src/components/homeBanner.tsx
+++ src/components/homeBanner.tsx
@@ -1,15 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
-import { JSX, useEffect, useState } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { JSX, useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useTheme } from "../services/theme";
-import { Loading } from "../services/animation";
 import { useSms } from "../services/messages";
 
 /**
  * Displays the transaction details on the home screen
  * @returns {JSX.Element} A reusable component that displays transaction summary
  */
-const HomeBanner = () => {
+const HomeBanner = (): JSX.Element => {
   const [income, setIncome] = useState(0); // TODO: Fetch annual income
   const [expense, setExpense] = useState(0); // TODO: Fetch annual expense
   const theme = useTheme();
@@ -75,7 +74,7 @@ const HomeBanner = () => {
       <View style={styles.balanceContainer}>
         <Text style={styles.title}>Estimated Balance</Text>
         <Text style={[styles.title, { fontSize: 30 }]}>
-          Ksh.{balance.toLocaleString("KE", { maximumFractionDigits: 0 })}
+          Ksh.{balance.toLocaleString("en-KE", { maximumFractionDigits: 0 })}
         </Text>
       </View>
       <View style={styles.summaryContainer}>
@@ -83,12 +82,13 @@ const HomeBanner = () => {
           {!isImporting ? (
             <MaterialIcons name="arrow-downward" style={styles.summaryIcon} />
           ) : (
-            <Loading color={"white"} size={30} />
+            <ActivityIndicator color={"white"} size={30} />
           )}
           <View style={styles.summaryTextContainer}>
             <Text style={styles.summaryText}>Income</Text>
             <Text style={styles.summaryText}>
-              Ksh. {income.toLocaleString("KE", { maximumFractionDigits: 0 })}
+              Ksh.{" "}
+              {income.toLocaleString("en-KE", { maximumFractionDigits: 0 })}
             </Text>
           </View>
         </View>
@@ -96,12 +96,13 @@ const HomeBanner = () => {
           {!isImporting ? (
             <MaterialIcons name="arrow-upward" style={styles.summaryIcon} />
           ) : (
-            <Loading color={"white"} size={30} />
+            <ActivityIndicator color={"white"} size={30} />
           )}
           <View style={styles.summaryTextContainer}>
             <Text style={styles.summaryText}>Outgoing</Text>
             <Text style={styles.summaryText}>
-              Ksh. {expense.toLocaleString("KE", { maximumFractionDigits: 0 })}
+              Ksh.{" "}
+              {expense.toLocaleString("en-KE", { maximumFractionDigits: 0 })}
             </Text>
           </View>
         </View>
