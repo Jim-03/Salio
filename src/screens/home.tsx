a9954587490f4c603555f2b00c8cc393e5542efe
Jim-03 <chemuku.jimmy@gmail.com>
2026-03-02 15:18:06 +0300

    feat: Enhance Home component with latest transactions display and styling

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,17 +1,64 @@
-import { View } from "react-native";
-import { JSX } from "react";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import { JSX, useEffect, useState } from "react";
 import HomeBanner from "../components/homeBanner";
 import ExpenseSummary from "../components/expenseSummary";
+import { getLast5Transactions, TransactionRecord } from "../utils/database";
+import { useDB } from "../services/database";
+import Review from "../components/review";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useTheme } from "../services/theme";
 
 /**
  * Home page
  * @returns {JSX.Element} A reusable component that renders the home page
  */
 const Home = (): JSX.Element => {
+  const [last5Transactions, setLast5Transactions] = useState<
+    TransactionRecord[]
+  >([]);
+  const db = useDB();
+  const theme = useTheme();
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLast5Transactions(await getLast5Transactions(db));
+    };
+    loadData();
+  }, []);
+
+  const styles = StyleSheet.create({
+    labelContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginVertical: 10,
+      paddingHorizontal: 5,
+    },
+    labelText: {
+      color: theme === "light" ? "seagreen" : "white",
+      fontWeight: "bold",
+      fontSize: 17,
+      opacity: 0.8,
+    },
+  });
   return (
     <View style={{ flex: 1 }}>
       <HomeBanner />
       <ExpenseSummary />
+      <View style={styles.labelContainer}>
+        <Text style={styles.labelText}>Latest Transactions</Text>
+        <TouchableWithoutFeedback>
+          <View style={{ flexDirection: "row", gap: 3, alignItems: "center" }}>
+            <Text style={styles.labelText}>View All</Text>
+            <MaterialCommunityIcons
+              name="hand-pointing-right"
+              size={24}
+              color={theme === "light" ? "seagreen" : "white"}
+              style={{ opacity: 0.8 }}
+            />
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+      <Review transactions={last5Transactions} />
     </View>
   );
 };
