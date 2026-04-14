11ca5ef88333ab16fd83ff386a67645ba0a9fedc
jim <chemuku.jimmy@gmail.com>
2026-04-14 17:24:25 +0300

    feat: Add color constants for theming support

---
+++ constants/colors.ts
@@ -0,0 +1,5 @@
+export const primaryColor = "rgb(46, 139, 87)";
+export const darkModeBackground = "rgb(25, 25, 25)";
+export const lightModeBackground = "rgb(245, 245, 245)";
+export const darkModeContainerColor = "rgb(44, 44, 44)";
+export const lightModeContainerColor = "rgb(255, 255, 255)";
