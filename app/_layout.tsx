73568a2f69e9f4bba947eb71a01030671d023e7a
jim <chemuku.jimmy@gmail.com>
2026-04-16 13:48:08 +0300

    fix: Add main app controller to handle redirection after layout component is injected

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,52 +1,20 @@
 import DatabaseProvider from "@/contexts/database-provider";
 import ThemeProvider from "@/contexts/theme-provider";
-import { Redirect, Stack } from "expo-router";
-import { AsyncStorage } from "expo-sqlite/kv-store";
-import { useEffect, useState, type JSX } from "react";
+import { Stack } from "expo-router";
+import { type JSX } from "react";
 
 /**
  * The applications main layout
  *
- * It checks if the app has ever been opened. If it's the first time,
- * the app redirects to the '/introduction' route to introduce the app to the user
- * It initializes a connection to the database for child components to use that connection
- * It also provides a theme for the child components
+ * Loads the app's database and theme providers for the entire app
  * @returns {JSX.Element} An element that controls how the app is launched
  * @todo Lock the user out in case of 1 minute of inactivity
  */
 export default function AppLayout(): JSX.Element {
-  const [isFirstTime, setIsFirstTime] = useState(false); // State to confirm if the app has ever been opened
-
-  useEffect(() => {
-    /**
-     * Confirm if the app has ever been opened before
-     */
-    const checkIfFirstTime = async () => {
-      try {
-        const data = await AsyncStorage.getItem("firstTime");
-
-        if (data) {
-          setIsFirstTime(JSON.parse(data));
-        }
-      } catch (error) {
-        // Default to first time if an error has occurred
-        console.error(
-          "An error has occurred while checking if the app has been opened before. Defaulting to first time",
-        );
-        console.error(error);
-      }
-    };
-    checkIfFirstTime();
-  }, []);
-
   return (
     <ThemeProvider>
       <DatabaseProvider>
-        {isFirstTime ? (
-          <Redirect href={"/introduction"} />
-        ) : (
-          <Stack screenOptions={{ headerShown: false }} />
-        )}
+        <Stack screenOptions={{ headerShown: false }} />
       </DatabaseProvider>
     </ThemeProvider>
   );
---
+++ app/index.tsx
@@ -0,0 +1,43 @@
+import { Redirect } from "expo-router";
+import AsyncStorage from "expo-sqlite/kv-store";
+import { useEffect, useState, type JSX } from "react";
+
+/**
+ * Main app controller
+ *
+ * It checks if the app has ever been opened
+ * If it's the first time, it redirects to the '/introduction' route to introduce the app to the user
+ * Otherwise, it redirects to the '/home' route where the main activity is
+ * @returns {JSX.Element} A redirect component
+ */
+export default function (): JSX.Element {
+  const [isFirstTime, setIsFirstTime] = useState(true); // State to confirm if the app has ever been opened
+
+  useEffect(() => {
+    /**
+     * Confirm if the app has ever been opened before
+     */
+    const checkIfFirstTime = async () => {
+      try {
+        const data = await AsyncStorage.getItem("firstTime");
+
+        if (data !== null) {
+          setIsFirstTime(JSON.parse(data));
+        }
+      } catch (error) {
+        // Default to first time if an error has occurred
+        console.error(
+          "An error has occurred while checking if the app has been opened before. Defaulting to first time",
+        );
+        console.error(error);
+      }
+    };
+    checkIfFirstTime();
+  }, []);
+
+  if (isFirstTime) {
+    return <Redirect href={"/introduction"} />;
+  }
+
+  return <Redirect href={"/main"} />;
+}
