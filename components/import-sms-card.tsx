4d50418037de6f13d88275cbf6d06573f0f9fc0c
jim <chemuku.jimmy@gmail.com>
2026-04-17 20:01:18 +0300

    feat: Modified to inform the user on how many transactions have been added

--- components/import-sms-card.tsx
+++ components/import-sms-card.tsx
@@ -1,62 +1,67 @@
-import { darkModeContainerColor, lightModeContainerColor, primaryColor } from "@/constants/colors";
+import {
+  darkModeContainerColor,
+  lightModeContainerColor,
+  primaryColor,
+} from "@/constants/colors";
 import { useLightTheme } from "@/contexts/theme-provider";
 import { ActivityIndicator, Modal, StyleSheet, Text, View } from "react-native";
 
 interface ImportSmsCardProps {
   isLoading: boolean;
+  transactions: number;
 }
 
 /**
  * Display a card to block user's from using the app during importation
  * @param {boolean} isLoading Boolean variable that determines if the card is visible
+ * @param transactions Number of transactions that have been added
  * @returns {React.JSX.Element} A card component to block users
  */
-export default function ImportSmsCard({ isLoading }: ImportSmsCardProps): React.JSX.Element {
+export default function ImportSmsCard({
+  isLoading,
+  transactions,
+}: ImportSmsCardProps): React.JSX.Element {
   const isLight = useLightTheme();
 
-  const bgColor = isLight ? 'rgba(0, 0, 0, 0.3)' : 'rgba(44, 44, 44, 0.7)';
+  const bgColor = isLight ? "rgba(0, 0, 0, 0.3)" : "rgba(44, 44, 44, 0.7)";
   const cardColor = isLight ? lightModeContainerColor : darkModeContainerColor;
-  const textColor = isLight ? primaryColor : 'white';
+  const textColor = isLight ? primaryColor : "white";
 
   return (
-      <Modal
-          transparent={true}
-          visible={isLoading}
-          animationType="fade"
-      >
-        <View style={[styles.background, { backgroundColor: bgColor }]}>
-          <View style={[styles.card, { backgroundColor: cardColor }]}>
-            <ActivityIndicator color={textColor} size={25} />
-            <Text style={[styles.info, { color: textColor }]}>
-              Importing new transactions
-            </Text>
-          </View>
+    <Modal transparent={true} visible={isLoading} animationType="fade">
+      <View style={[styles.background, { backgroundColor: bgColor }]}>
+        <View style={[styles.card, { backgroundColor: cardColor }]}>
+          <ActivityIndicator color={textColor} size={25} />
+          <Text style={[styles.info, { color: textColor }]}>
+            {transactions} {transactions === 1 ? "has" : "have"} been imported
+          </Text>
         </View>
-      </Modal>
+      </View>
+    </Modal>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   card: {
     height: 150,
-    width: '80%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "center",
     gap: 20,
     borderRadius: 20,
     elevation: 5,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
   },
   info: {
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
   },
 });
