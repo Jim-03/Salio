f3679005d2d55d3324d9952a6369b36468d22aa1
jim <chemuku.jimmy@gmail.com>
2026-09-08 16:00:09 +0300

    feat: Include all source files located in `/app` directory in tailwind compilation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ import nativewind from "nativewind/preset";
 
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [nativewind],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
