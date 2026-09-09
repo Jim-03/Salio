6f4cdf36082717bca715b29a0751d292da51772e
jim <chemuku.jimmy@gmail.com>
2026-09-09 17:47:58 +0300

    feat: Add base home screens

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,15 +1,50 @@
 import SystemDataProvider from "@/providers/data-provider.component";
 import { Tabs } from "expo-router";
 import AuthenticationProvider from "@/providers/authentication-provider.component";
+import Lucide from "@react-native-vector-icons/lucide";
 
 /**
  * Main layout for dashboard screens
  */
 export default function DashboardLayout() {
+  const iconSize = 24;
   return (
     <AuthenticationProvider>
       <SystemDataProvider>
-        <Tabs />
+        <Tabs>
+          <Tabs.Screen
+            name={"index"}
+            options={{
+              title: "Home",
+              tabBarIcon: () => <Lucide name={"home"} size={iconSize} />,
+            }}
+          />
+          <Tabs.Screen
+            name={"analysis"}
+            options={{
+              title: "Analysis",
+              tabBarIcon: () => (
+                <Lucide name={"chart-no-axes-column"} size={iconSize} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name={"history"}
+            options={{
+              title: "Transactions",
+              tabBarIcon: () => (
+                <Lucide name={"arrow-left-right"} size={iconSize} />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name={"settings"}
+            options={{
+              title: "Settings",
+              tabBarIcon: () => <Lucide name={"settings"} size={iconSize} />,
+            }}
+          />
+        </Tabs>
       </SystemDataProvider>
     </AuthenticationProvider>
   );
