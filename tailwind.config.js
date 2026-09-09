e5c1bedb744c0262619341ec31ec0348f27ee6b0
jim <chemuku.jimmy@gmail.com>
2026-09-09 15:46:13 +0300

    feat: Add `/component` directory for tailwind compilation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import nativewind from "nativewind/preset";
 
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [nativewind],
   theme: {
     extend: {},
