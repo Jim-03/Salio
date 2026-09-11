8f599bf668193dd91bd39b3142561aae24d51829
jim <chemuku.jimmy@gmail.com>
2026-09-11 12:54:52 +0300

    feat: Add model to track SMS addresses

--- src/config/models.py
+++ src/config/models.py
@@ -1,3 +1,11 @@
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
+class Address(Base):
+    __tablename__ = "tracked_addresses"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    address = Column(String, nullable=False, unique=True)
+    description = Column(String)
