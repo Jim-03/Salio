3af22379ad1f4eb84cfc45d5c979b2a279d9c699
jim <chemuku.jimmy@gmail.com>
2026-09-12 12:20:15 +0300

    feat: Add authorisation header

--- lib/client.ts
+++ lib/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const client = axios.create({
   headers: {
-    Authorization: process.env.EXPO_PUBLIC_AUTHORIZATION_KEY,
+    Authorization: "Bearer " + process.env.EXPO_PUBLIC_AUTHORIZATION_KEY,
   },
   baseURL: process.env.EXPO_PUBLIC_SERVER_URL,
 });
